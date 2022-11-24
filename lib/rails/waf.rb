b35edbc497beb9729b90700e3dd27e86d48fd634
Greg Molnar <molnargerg@gmail.com>
2022-11-24 06:46:05 +0100

    ditch the engine and move to a method for enable the waf

--- lib/rails/waf.rb
+++ lib/rails/waf.rb
@@ -1,10 +1,19 @@
 require "rack/attack"
 require "rails/waf/version"
 require "rails/waf/railtie"
-require "rails/waf/engine"
 
 module Rails
   module Waf
     # Your code goes here...
+    def self.enable!
+      ::Rack::Attack.enabled = true
+      ::Rack::Attack.class_eval do
+          blocklist("block scanners") do |request|
+            [
+              '(hydra)', '.nasl', 'absinthe', 'advanced email extractor', 'arachni', 'BFAC', 'brutus', 'bsqlbf', 'burpcollaborator', 'cisco-torch', 'commix', 'Detectify', 'dirbuster', 'domino hunter', 'dotdotpwn', 'F-Secure Radar', 'gobuster', 'havij', 'masscan', 'nessus', 'netsparker', 'nikto', 'nmap', 'nsauditor', 'Nuclei', 'openvas', 'pangolin', 'qualys', 'sqlmap', 'whatweb', 'WPScan', 'xmlrpc exploit'
+            ].any?{ |user_agent| request.user_agent =~ Regexp.new(user_agent)  }
+          end
+      end
+    end
   end
 end
